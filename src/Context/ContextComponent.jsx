9232cbbe8b37c788fdb5b1e65b4e66c11ccaa855
AbdullahFaiaz <talha0184999@gmail.com>
2024-05-31 18:09:22 +0600

    set loading in observer and enabled in tanstack query

--- src/Context/ContextComponent.jsx
+++ src/Context/ContextComponent.jsx
@@ -114,13 +114,15 @@ const ContextComponent = ({ children }) => {
         .then(res => {
           if(res.data.token){
             localStorage.setItem('access-token', res.data.token)
+            setLoading(false);
           }
         })
       }
       else{
         localStorage.removeItem('access-token')
+        setLoading(false);
       }
-      setLoading(false);
+      // setLoading(false);
     });
     return () => {
       
